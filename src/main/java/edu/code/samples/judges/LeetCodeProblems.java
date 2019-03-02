152e10f657d19dc0c2beabc544756418f8435109
Varun Sharma <varunshr@amazon.com>
2019-03-02 20:20:44 +0530

    Added code for coin change problems.

--- src/main/java/edu/code/samples/judges/LeetCodeProblems.java
+++ src/main/java/edu/code/samples/judges/LeetCodeProblems.java
@@ -1802,7 +1802,7 @@ public class LeetCodeProblems {
      * The length of Profits array and Capital array will not exceed 50,000.
      * The answer is guaranteed to fit in a 32-bit signed integer.
      */
-    public static class IPO {
+    static class IPO {
 
         static class Project {
             int capital;
@@ -1919,6 +1919,105 @@ public class LeetCodeProblems {
                 }
                 return -1;
             }
+        }
+    }
+
+
+    /**
+     * https://leetcode.com/problems/coin-change-2/
+     * You are given coins of different denominations and a total amount of money.
+     * Write a function to compute the number of combinations that make up that amount.
+     * You may assume that you have infinite number of each kind of coin.
+     *
+     * Example 1:
+     *
+     * Input: amount = 5, coins = [1, 2, 5]
+     * Output: 4
+     * Explanation: there are four ways to make up the amount:
+     * 5=5
+     * 5=2+2+1
+     * 5=2+1+1+1
+     * 5=1+1+1+1+1
+     * Example 2:
+     *
+     * Input: amount = 3, coins = [2]
+     * Output: 0
+     * Explanation: the amount of 3 cannot be made up just with coins of 2.
+     */
+    class CoinChange2 {
+
+        public int change(int amount, int[] coins) {
+            int[] dp = new int[amount + 1];
+            dp[0] = 1;
+            for (int coin : coins) {
+                for (int i = coin; i <= amount; i++) {
+                    dp[i] += dp[i-coin];
+                }
+            }
+            return dp[amount];
+        }
+    }
+
+
+    /**
+     * https://leetcode.com/problems/coin-change/
+     *You are given coins of different denominations and a total amount of money amount.
+     * Write a function to compute the fewest number of coins that you need to make up that amount.
+     * If that amount of money cannot be made up by any combination of the coins, return -1.
+     *
+     * Example 1:
+     *
+     * Input: coins = [1, 2, 5], amount = 11
+     * Output: 3
+     * Explanation: 11 = 5 + 5 + 1
+     * Example 2:
+     *
+     * Input: coins = [2], amount = 3
+     * Output: -1
+     */
+    class CoinChange {
+        public int coinChange(int[] coins, int amount) {
+            return tabulated(coins, amount);
+        }
+
+        private int tabulated2D(int[] coins, int amount) {
+            int[][] dp = new int[coins.length][amount+1];
+
+            for (int i=0; i<dp.length; i++) {
+                for (int j=0; j < dp[0].length; j++) {
+                    if (j==0) {
+                        dp[i][j] = 0;
+                    } else if(i==0 && j % coins[0] == 0) {
+                        dp[i][j] = j/coins[0];
+                    } else {
+                        dp[i][j] = Integer.MAX_VALUE;
+                    }
+                }
+            }
+
+            for (int i=1; i < dp.length; i++) {
+                for (int j=1; j < dp[0].length; j++) {
+                    dp[i][j] = Math.min(dp[i-1][j], Integer.MAX_VALUE);
+                    if (j >= coins[i] && dp[i][j-coins[i]] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j-coins[i]]+ 1);
+                    }
+                }
+            }
+            return dp[coins.length-1][amount] == Integer.MAX_VALUE ? - 1: dp[coins.length-1][amount];
+        }
+
+        private int tabulated(int[] coins, int amount) {
+            int[] cache = new int[amount+1];
+            cache[0] = 0;
+            for (int i=1; i<=amount; i++) {
+                cache[i] = Integer.MAX_VALUE;
+                for (int coin: coins) {
+                    if (i >= coin && cache[i-coin] != Integer.MAX_VALUE) {
+                        cache[i] = Math.min(cache[i], cache[i-coin] + 1);
+                    }
+                }
+            }
+            return cache[amount] == Integer.MAX_VALUE ? -1 : cache[amount];
         }
     }
 }
